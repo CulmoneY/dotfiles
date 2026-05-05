a60597d2f3a4abdcbb86a0d5e209d2536d6d5480
CulmoneY <yculmone@rogers.com>
2026-05-05 01:17:00 -0400

    [Nvim] Plugins Refactor

---
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,37 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000,
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false },
+        },
+      }
+
+      vim.cmd.colorscheme 'tokyonight-night'
+    end,
+  },
+  {
+    'nvim-mini/mini.nvim',
+    config = function()
+      require('mini.ai').setup {
+        mappings = {
+          around_next = 'aa',
+          inside_next = 'ii',
+        },
+        n_lines = 500,
+      }
+
+      require('mini.surround').setup()
+
+      local statusline = require 'mini.statusline'
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function() return '%2l:%-2v' end
+    end,
+  },
+}
